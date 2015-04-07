f9b526582d2b3997c5d042f97020e2344e275002
Tom Malary <tom@Toms-MacBook-Air.local>
2015-04-07 13:28:32 +0300

    Initial Commit

---
+++ YOUTIR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YOUTIR
+//
+//  Created by Tom Malary on 4/7/15.
+//  Copyright (c) 2015 YOUTIR LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
