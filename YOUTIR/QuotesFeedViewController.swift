0a5e68057163f6b582c61c68255e7591157a3136
Tom Malary <tom@Toms-MacBook-Air.local>
2015-04-11 23:01:24 +0300

    RequestFormVC and QuotesFeedVC added

---
+++ YOUTIR/QuotesFeedViewController.swift
@@ -0,0 +1,53 @@
+//
+//  QuotesFeedViewController.swift
+//  YOUTIR
+//
+//  Created by Tom Malary on 4/11/15.
+//  Copyright (c) 2015 YOUTIR LLC. All rights reserved.
+//
+
+import UIKit
+
+class QuotesFeedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    //fake style cell, just to let it compile
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        return cell
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
