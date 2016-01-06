7185e5fb626b783c364d3bf7d27fcbe1bd1ed89b
Tom Nutwood <youtir.com@gmail.com>
2016-01-06 23:03:03 +0400

    test HTTP Request

--- YOUTIR/RequestFormTableViewController.swift
+++ YOUTIR/RequestFormTableViewController.swift
@@ -22,9 +22,39 @@ class RequestFormTableViewController: UITableViewController, UITextFieldDelegate
     @IBOutlet var heightTextField: UITextField!
     @IBOutlet var insuranceTextField: UITextField!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let json = [
+            "title": "Степное"
+        ]
+        
+        let jsonData = try! NSJSONSerialization.dataWithJSONObject(json, options: [])
+        
+        // Config Basic Auth
+        let username = "malbaq"
+        let password = "8F102968198C7999F7AF8840FE7B44D2F4BBAD81"
+        let loginString = NSString(format: "%@:%@", username, password)
+        let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
+        let base64LoginString = loginData.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.init(rawValue: 0))
+        
+        // Create, config and perfom HTTP Request
+        let url = NSURL(string: "https://kabinet.pecom.ru/api/v1/BRANCHES/FINDBYTITLE/")
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json;charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        request.HTTPBody = jsonData
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+        
+            if let response = response {
+                print("the response is \(response)")
+            }
+        }
+        
+        task.resume()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
